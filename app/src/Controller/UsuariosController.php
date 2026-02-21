f1695f5166c2906ca313ffc4811afc969a481f1c
IagoFP <iagofranciscoperez123@gmail.com>
2026-02-21 19:07:44 +0100

    Agregada validacion de campos vacios a users

--- app/src/Controller/UsuariosController.php
+++ app/src/Controller/UsuariosController.php
@@ -14,6 +14,7 @@ use Symfony\Component\Routing\Exception\MethodNotAllowedException;
 
 class UsuariosController extends AbstractController
 {
+    //Ruta para obtener los usuarios dados de alta en la aplicacion
     #[Route('/users', name: 'get_users', methods: ['GET'])]
     public function getUsers(Request $request, EntityManagerInterface $entitymanager): Response
     {
@@ -46,23 +47,57 @@ class UsuariosController extends AbstractController
         return $response;
     }
 
+    //Ruta para dar de alta a un usuario en la app
     #[Route('/users', name: 'post_users', methods: ['POST'])]
     public function addUser(Request $request, EntityManagerInterface $entityManager): Response
     {
         $data = json_decode($request->getContent(), true);
-        $usuario = new Usuarios();
 
+        if (!$data) {
+            return $this->json(['error' => 'JSON inválido'], 400);
+        }
+
+        $errors = [];
+
+        if (empty($data['username'])) {
+            $errors['username'] = "El campo username es obligatorio";
+        }
+
+        if (empty($data['email'])) {
+            $errors['email'] = "El campo email es obligatorio";
+        }
+
+        if (empty($data['pass'])) {
+            $errors['pass'] = "El campo pass es obligatorio";
+        }
+
+        if (!isset($data['salario'])) {
+            $errors['salario'] = "El campo salario es obligatorio";
+        }
+
+        if (!isset($data['disponible'])) {
+            $errors['disponible'] = "El campo disponible es obligatorio";
+        }
+
+        if (!isset($data['ahorros'])) {
+            $errors['ahorros'] = "El campo ahorros es obligatorio";
+        }
+
+        if (!empty($errors)) {
+            return $this->json($errors, 400);
+        }
+
+        $usuario = new Usuarios();
         $usuario->setUsername($data['username']);
         $usuario->setEmail($data['email']);
         $usuario->setPass(password_hash($data['pass'], PASSWORD_DEFAULT));
         $usuario->setSalario($data['salario']);
         $usuario->setSalarioDisponible($data['disponible']);
         $usuario->setAhorro($data['ahorros']);
+
         $entityManager->persist($usuario);
         $entityManager->flush();
 
-        $response = $this->json('Usuario creado con exito', 201);
-
-        return $response;
+        return $this->json('Usuario creado con éxito', 201);
     }
 }
